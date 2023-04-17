ee0ea0cb5484c702356252361a6a64e1e5bf8ae4
aalexandr6 <aalexanderp6@gmail.com>
2023-04-17 18:05:57 -0500

    db

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,5 +1,5 @@
 const { connect, connection } = require('mongoose');
-const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/your-database-name';
+const connectionString = process.env.MONGO_URI || 'mongodb://localhost:27017/socialDB';
 
 connect(connectionString);
 
