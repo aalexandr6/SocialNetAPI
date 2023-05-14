466d926e60552cfe3565a6c45dc4aa854b2e84be
Aaron Padilla <codewizard@Aarons-MacBook-Air-2.local>
2023-05-14 19:03:44 -0400

    fixing error

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -14,4 +14,4 @@ users.route("/").get(getUsers).post(createUser);
 users.route("/:id").get(getOneUser).put(updateUser).delete(deleteUser);
 users.route("/:userId/friends/:friendId").post(addFriend).delete(deleteFriend);
 
-
+module.exports = users;
